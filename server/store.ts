a5d647f31d5cd8c612fdcbc1210bef12eb8c980b
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-05-21 09:12:32 +1200

    add initial-data and store.ts

---
+++ server/store.ts
@@ -0,0 +1,10 @@
+import type { Puppy } from '../models/Puppy.ts'
+import initialData from './initial-data.ts'
+
+interface Data {
+  puppies: Puppy[]
+}
+
+export async function getPuppies(): Promise<Data> {
+  return initialData
+}
